fca1e935567f37e36acd6485c031f09c8f6be112
Tarık <feanorwastaken@gmail.com>
2022-10-06 23:28:08 +0300

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+function Navbar() {
+  return(
+    <nav>
+      <img src='./images/airbnb1.png' alt="img" className="nav--logo"/>
+    </nav>
+  )
+}
+
+export default Navbar;
---
+++ src/data.js
@@ -0,0 +1,132 @@
+export default [
+    {
+        id: 1,
+        title: "Life Lessons with Katie Zaferes",
+        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        price: 136,
+        coverImg: "./images/katie.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Learn Wedding Photography",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 125,
+        coverImg: "./images/wedding.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Group Mountain Biking",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "./images/bike.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Norway",
+        openSpots: 3,
+    },
+    {
+        id: 4,
+        title: "Discover Sunny Fort Lauderdale",
+        description: "Want to know the scoop on anything from celebrity mansions to local lore and more? We’ll help you plan a special trip to Fort Lauderdale and perhaps beyond.",
+        price: 100,
+        coverImg: "./images/florida.webp",
+        stats: {
+            rating: 5.0,
+            reviewCount: 7
+        },
+        location: "USA, Florida",
+        openSpots: 15,
+    },
+    {
+        id: 5,
+        title: "Make Italian Grandma's Pasta",
+        description: "Cook Pasta from scratch with grandma, let her share with you her culinary secrets and the traditional authentic pasta technique. Listen to her stories and be ready to be involved in our community.",
+        price: 55,
+        coverImg: "./images/grandma.webp",
+        stats: {
+            rating: 4.7,
+            reviewCount: 3262
+        },
+        location: "Online",
+        openSpots: 100,
+    },
+    {
+        id: 6,
+        title: "Murder Mystery Escape Room Game",
+        description: "There was a murder! Oh no! And only You as a group of detectives are able to solve it... Will you make it?",
+        price: 25,
+        coverImg: "./images/murder.webp",
+        stats: {
+            rating: 4.8,
+            reviewCount: 343
+        },
+        location: "Online",
+        openSpots: 25,
+    },
+    {
+        id: 7,
+        title: "Tarot Reading and Meditation Spiritual Guidance",
+        description: "We will begin with a meditation to quiet our minds and make it receptive and highly tuned to the unseen world, where I will teach you how to connect to your intuition.",
+        price: 27,
+        coverImg: "./images/tarot.webp",
+        stats: {
+            rating: 4.7,
+            reviewCount: 3262
+        },
+        location: "Online",
+        openSpots: 15,
+    },
+    {
+        id: 8,
+        title: "Healing concert",
+        description: "Healing session is not only passive but also interactive.",
+        price: 30,
+        coverImg: "./images/concert.webp",
+        stats: {
+            rating: 4.9,
+            reviewCount: 42
+        },
+        location: "Online",
+        openSpots: 46,
+    },
+    {
+        id: 9,
+        title: "Discover Buenos Aires' Street Art & Sketch with a Specialist",
+        description: "Discover Buenos Aires' Street Art from the comfort of your home guided by a contemporary art curator, photographer and designer and connect with your inner artist!",
+        price: 35,
+        coverImg: "./images/art.webp",
+        stats: {
+            rating: 4.7,
+            reviewCount: 29
+        },
+        location: "Online",
+        openSpots: 24,
+    }, 
+     {
+          id: 10,
+          title: "Paella & Sangria with a Top Chef / Great for Team Building",
+          description: "You will do Real Paella all from scratch, following step by step our family recipe, exactly as we did in our family restaurant!",
+          price: 36,
+          coverImg: "./images/kitchen.webp",
+          stats: {
+              rating: 4.9,
+              reviewCount: 55
+          },
+          location: "Online",
+          openSpots: 54,
+      }
+]
