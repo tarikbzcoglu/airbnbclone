792967ec923cfa0dfce244e06e0ac193e7298595
Tarık <feanorwastaken@gmail.com>
2022-10-06 23:27:38 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Card from './components/Card'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import data from './data'
 
-function App() {
+export default function App() {
+  const cards = data.map(function(item) {
+    return (
+      <Card
+      key={item.id}
+      item= {item}
+       />
+    )
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Hero />
+      <section className="card--list">
+      {cards}
+      </section>
     </div>
   );
 }
 
-export default App;
